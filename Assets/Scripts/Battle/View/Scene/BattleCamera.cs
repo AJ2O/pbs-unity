b4c6b2593e09ce64f28f17c6edceaffc96619d22
Juwon44 <juwonobat@gmail.com>
2020-12-01 09:54:02 -0500

    #8: Finished documenting PBS.Battle.View.Scene.BattleCamera

--- Assets/Scripts/Battle/View/Scene/BattleCamera.cs
+++ Assets/Scripts/Battle/View/Scene/BattleCamera.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 namespace PBS.Battle.View.Scene
 {
+    /// <summary>
+    /// A camera meant to be used for viewing the <seealso cref="Canvas"/>.
+    /// </summary>
     public class BattleCamera : MonoBehaviour
     {
+        #region Attributes
+        /// <summary>
+        /// The Unity Camera to use.
+        /// </summary>
         public UnityEngine.Camera mainCamera;
+        /// <summary>
+        /// The default/resting position of the camera.
+        /// </summary>
         public Vector3 defaultPosition;
+        #endregion
 
         private void Awake()
         {
