cbafb4451fa302a25a790e4588d0125f41962df1
Juwon44 <juwonobat@gmail.com>
2020-12-01 10:22:07 -0500

    #8: Finished documenting PBS.Battle.View.Scene.Entities.BaseEntity

--- Assets/Scripts/Battle/View/Scene/Entities/BaseEntity.cs
+++ Assets/Scripts/Battle/View/Scene/Entities/BaseEntity.cs
@@ -4,25 +4,48 @@ using UnityEngine;
 
 namespace PBS.Battle.View.Scene.Entities
 {
+    /// <summary>
+    /// The base class for objects meant to be drawn on the <seealso cref="Canvas"/>.
+    /// </summary>
     public class BaseEntity : MonoBehaviour
     {
+        #region Attributes
+        /// <summary>
+        /// The main sprite renderer.
+        /// </summary>
         public SpriteRenderer spriteRenderer;
+        /// <summary>
+        /// A shadow sprite matching the shape of <seealso cref="spriteRenderer"/>.
+        /// </summary>
         public SpriteRenderer shadowRenderer;
 
+        /// <summary>
+        /// Used for modifying properties of the sprite drawn in <seealso cref="spriteRenderer"/>.
+        /// </summary>
         private MaterialPropertyBlock _propBlock;
+        #endregion
+
         void Awake()
         {
             _propBlock = new MaterialPropertyBlock();
         }
 
+        /// <summary>
+        /// Scales the size of this object by the specified properties.
+        /// </summary>
+        /// <param name="scaleAll">The amount to scale all the axes by.</param>
+        /// <param name="scaleX">The amount to scale the X-size by.</param>
+        /// <param name="scaleY">The amount to scale the Y-size by.</param>
+        /// <param name="scaleZ">The amount to scale the Z-size by.</param>
+        /// <param name="additive">If false, the object's scale is reset to (1,1,1) before applying scaling.</param>
         public void Scale(
             float scaleAll = 1f,
             float scaleX = 1f, float scaleY = 1f, float scaleZ = 1f, 
-            bool absolute = true)
+            bool additive = true)
         {
-            float newX = (absolute ? transform.localScale.x : 1f) * scaleAll * scaleX;
-            float newY = (absolute ? transform.localScale.y : 1f) * scaleAll * scaleY;
-            float newZ = (absolute ? transform.localScale.z : 1f) * scaleAll * scaleZ;
+            float newX = (additive ? transform.localScale.x : 1f) * scaleAll * scaleX;
+            float newY = (additive ? transform.localScale.y : 1f) * scaleAll * scaleY;
+            float newZ = (additive ? transform.localScale.z : 1f) * scaleAll * scaleZ;
             transform.localScale = new Vector3(newX, newY, newZ);
         }
     }
