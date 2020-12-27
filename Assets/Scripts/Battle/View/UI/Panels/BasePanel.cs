501aa318373135944cce3bbebc91a16ebe6e1c9c
Juwon44 <juwonobat@gmail.com>
2020-12-27 16:29:18 -0500

    Documented half of PBS.Battle.View.UI.Panels

--- Assets/Scripts/Battle/View/UI/Panels/BasePanel.cs
+++ Assets/Scripts/Battle/View/UI/Panels/BasePanel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 namespace PBS.Battle.View.UI.Panels
 {
+    /// <summary>
+    /// The base component for displaying UI panels.
+    /// </summary>
     public class BasePanel : MonoBehaviour
     {
         public virtual void ClearSelf() { }
--- Assets/Scripts/Battle/View/UI/Panels/Command/Command.cs
+++ Assets/Scripts/Battle/View/UI/Panels/Command/Command.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 
 namespace PBS.Battle.View.UI.Panels
 {
+    /// <summary>
+    /// This component handles the UI menu fir command selection.
+    /// </summary>
     public class Command : BasePanel
     {
+        #region Attributes
         [Header("Prompts")]
         public GameObject promptBox;
         public Text promptText;
@@ -17,8 +21,15 @@ namespace PBS.Battle.View.UI.Panels
             bagBtn,
             runBtn;
         public BTLUI_Button backBtn;
+        #endregion
 
-        public void SetCommands(PBS.Battle.View.WifiFriendly.Pokemon pokemon, IEnumerable<BattleCommandType> commandList)
+        #region Commands
+        /// <summary>
+        /// Sets the commands to be displayed on this component.
+        /// </summary>
+        /// <param name="pokemon">The pokemon to display commands for.</param>
+        /// <param name="commandList">The list of commands to display.</param>
+        public void SetCommands(WifiFriendly.Pokemon pokemon, IEnumerable<BattleCommandType> commandList)
         {
             HashSet<BattleCommandType> commandSet = new HashSet<BattleCommandType>(commandList);
             fightBtn.gameObject.SetActive(commandSet.Contains(BattleCommandType.Fight));
@@ -28,7 +39,10 @@ namespace PBS.Battle.View.UI.Panels
             backBtn.gameObject.SetActive(commandSet.Contains(BattleCommandType.Back));
             promptText.text = "What will <color=yellow>" + pokemon.nickname + "</color> do?";
         }
-
+        /// <summary>
+        /// Highlights the button matching the given command type, and unhighlights the rest.
+        /// </summary>
+        /// <param name="commandType">The command type to match.</param>
         public void HighlightCommand(BattleCommandType commandType)
         {
             fightBtn.UnselectSelf();
@@ -63,15 +77,23 @@ namespace PBS.Battle.View.UI.Panels
                     break;
             }
         }
+
+        /// <summary>
+        /// Highlights the back button on the UI.
+        /// </summary>
         public void HighlightBackButton()
         {
             HighlightCommand(BattleCommandType.None);
             backBtn.SelectSelf();
         }
-
-        public void PromptPokemon(PBS.Battle.View.WifiFriendly.Pokemon pokemon)
+        /// <summary>
+        /// Displays a message prompt for the given Pokemon.
+        /// </summary>
+        /// <param name="pokemon"></param>
+        public void PromptPokemon(WifiFriendly.Pokemon pokemon)
         {
             promptText.text = "What will " + pokemon.nickname + " do?";
         }
+        #endregion
     }
 }
