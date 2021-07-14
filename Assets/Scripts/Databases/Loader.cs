d77d3c949c99f76e46e911c6a76c79c2d441d304
Juwon44 <juwonobat@gmail.com>
2021-07-13 23:11:08 -0400

    #52: added nugetforunity package

---
+++ Assets/Scripts/Databases/Loader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Newtonsoft.Json;
+
+namespace PBS.Databases
+{
+    public class Loader
+    {
+        //create an object of SingleObject
+        private static Loader singleton = new Loader();
+
+        //make the constructor private so that this class cannot be
+        //instantiated
+        private Loader() { }
+
+        //Get the only object available
+        public static Loader instance
+        {
+            get
+            {
+                return singleton;
+            }
+            private set
+            {
+                singleton = value;
+            }
+        }
+
+        // Pokemon
+        private Dictionary<string, PBS.Data.Pokemon> pokemonDB = new Dictionary<string, PBS.Data.Pokemon>();
+        public PBS.Data.Pokemon LoadPokemon(string ID)
+        {
+            // Load from files
+            //PBS.Data.Pokemon pk = New
+
+            return null;
+        }
+        public PBS.Data.Pokemon GetPokemonData(string ID)
+        {
+            if (pokemonDB.ContainsKey(ID))
+            {
+                return pokemonDB[ID];
+            }
+            Debug.LogWarning("Could not find Pokemon with ID: " + ID + " in DB cache.");
+            return LoadPokemon(ID);
+        }
+    }
+}
